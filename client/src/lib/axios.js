ea11697690b94cc40eb85de43b39a9bedf92c4c7
JOSHUA <joshuannorom2005@gmail>
2025-09-30 22:07:59 +0100

    first commit

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://mern-my-notes-server.vercel.app"
+    baseURL: "http://localhost:5001/api"
 })
 
 export default axiosInstance;
--- server/src/routes/noteRoutes.js
+++ server/src/routes/noteRoutes.js
@@ -9,7 +9,7 @@ import {
 
 const router = express.Router();
 
-router.get("/home", getAllNotes);
+router.get("/", getAllNotes);
 router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
